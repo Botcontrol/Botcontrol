0bf6869694ef5992d52b619e9b77695ac93e4500
Botcontrol <acefltrade@gmail.com>
2014-11-26 17:56:47 +0300

    26/11/2014

---
+++ CheckResponseContract/ICheckControlContract.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CheckResponseContract
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "IService1" в коде и файле конфигурации.
+    [ServiceContract]
+    public interface ICheckControlCotract
+    {
+        [OperationContract]
+        List<CheckResponse> GetData();
+
+    }
+
+     [DataContract]
+    public class CheckResponse
+    {   
+        [DataMember]
+        public string FolderName { get; set; }
+
+        [DataMember]
+        public TimeSpan CheckTime { get; set; }
+       
+        [DataMember]
+        public string CodeNumber { get; set; }
+     
+        public CheckResponse(string folderName, TimeSpan checkTime, string codeNumber)
+        {
+            FolderName = folderName;
+            CheckTime = checkTime;
+            CodeNumber = codeNumber;
+        }
+    }
+    
+}
